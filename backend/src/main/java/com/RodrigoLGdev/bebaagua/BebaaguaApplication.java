06fde4b654ce6bf451b3a1d716a1c77d0c9fe8c2
Luiz Guilherme Anduze <l.g.anduze@gmail.com>
2023-03-04 18:28:45 -0300

    backend project created

---
+++ backend/src/main/java/com/RodrigoLGdev/bebaagua/BebaaguaApplication.java
@@ -0,0 +1,13 @@
+package com.RodrigoLGdev.bebaagua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebaaguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebaaguaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/RodrigoLGdev/bebaagua/BebaaguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RodrigoLGdev.bebaagua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebaaguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
